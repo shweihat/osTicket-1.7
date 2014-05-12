1e8872b6d99938ec69877d89feda53e5118bb985
shweihat <shweihat@gmail.com>
2014-05-12 01:29:21 +0100

    changed index.php

--- index.php
+++ index.php
@@ -25,20 +25,20 @@ require(CLIENTINC_DIR.'header.inc.php');
         echo  '<h1>Welcome to the Support Center</h1>';
     ?>
     <div id="new_ticket">
-        <h3>Open A New Ticket</h3>
+        <h3>Open A New Incident</h3>
         <br>
         <div>Please provide as much detail as possible so we can best assist you. To update a previously submitted ticket, please login.</div>
         <p>
-            <a href="open.php" class="green button">Open a New Ticket</a>
+            <a href="open.php" class="green button">Open a New Incident</a>
         </p>
     </div>
 
     <div id="check_status">
-        <h3>Check Ticket Status</h3>
+        <h3>Check Incident Status</h3>
         <br>
         <div>We provide archives and history of all your current and past support requests complete with responses.</div>
         <p>
-            <a href="view.php" class="blue button">Check Ticket Status</a>
+            <a href="view.php" class="blue button">Check Incident Status</a>
         </p>
     </div>
 </div>
@@ -47,7 +47,7 @@ require(CLIENTINC_DIR.'header.inc.php');
 if($cfg && $cfg->isKnowledgebaseEnabled()){
     //FIXME: provide ability to feature or select random FAQs ??
 ?>
-<p>Be sure to browse our <a href="kb/index.php">Frequently Asked Questions (FAQs)</a>, before opening a ticket.</p>
+<p>Be sure to browse our <a href="kb/index.php">Frequently Asked Questions (FAQs)</a>, before opening an incident.</p>
 </div>
 <?php
 } ?>
